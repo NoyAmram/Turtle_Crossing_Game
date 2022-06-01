a6ba7efee00a15df62182fd470ebd539eac2bbac
NoyAmram <98146080+NoyAmram@users.noreply.github.com>
2022-06-01 07:51:22 +0300

    Initial upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+timmy = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(timmy.move_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(timmy) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # Detect successful crossing
+    if timmy.is_at_finish_line():
+        timmy.refresh()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
+
+
